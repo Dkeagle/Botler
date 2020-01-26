884e8dcf4fc1f3f6e06712a44c5401b02cccc6d7
Dylan TAILDEMAN <49946516+Dkeagle@users.noreply.github.com>
2020-01-26 17:31:14 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# MogBot Config File
+# Dylan "Dkeagle" TAILDEMAN
+
+# Variables
+admin_id_list = [301442707740688384]
+bot_name = "Mog"
+bot_prefix = "!"
+bot_token = "NTcwNTY4MjMyNDY2MTIwNzA1.Xi27aw.rbW_Uop9oMNhm2-LUHmHMhIj2_8"
